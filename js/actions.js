0c9695a4dd87e47c9c4b9006e629b0693103bdef
joaquinst <integradb.mx@gmail.com>
2014-12-06 12:11:09 -0600

    ui, configuracion y notificaciones

---
+++ js/actions.js
@@ -0,0 +1,24 @@
+var fn = {
+    ready: function(){
+        document.addEventListener('deviceready',fn.ready,false);
+    },
+    init: function(){
+        $('#regSend').click(fn.enviarRegistro);
+        var x = false;
+        if(!x){
+            window.location.href = '#reg';
+        }
+    },
+    enviarRegistro: function(){
+        var nom = $('#regNom').val(); // val('asigna') // $('#regNom').val().val('asigna')
+        var tel = $('#regTel').val();
+        var mail = $('#regMail').val();
+        if(nom !='' && tel !='' && mail !='' ){
+            navigator.notification.alert('Correcto',null,'Felicidades','Aceptar');
+        }else{
+            alert('Todos los campos son requeridos');        
+        }
+    }
+};
+
+$(fn.ready);
