9e3c8a8ee7e6ff96690961aaa8cd1eb0122a54b8
joaquinst <integradb.mx@gmail.com>
2014-12-13 11:51:50 -0600

    1312.02

--- js/actions.js
+++ js/actions.js
@@ -6,26 +6,28 @@ var fn = {
         $('#regSend').click(fn.enviarRegistro);
         $('#regFoto').click(capture.imgCapture);
         var x = false;
-        if(!x){
+        if(!x)
             window.location.href = '#reg';
-        }
     },
     enviarRegistro: function(){
         var nom = $('#regNom').val();
         var tel = $('#regTel').val();
         var mail = $('#regMail').val();
         var foto = $('#regFoto').attr('rel');
-        if(nom !='' && tel !='' && mail !='' && foto!='' && foto != undefined){
+        
+        if(nom != '' && tel != '' && mail != '' && foto != '' && foto != undefined){
             $.ajax({
                 type: "POST",
                 url: "http://carlos.igitsoft.com/apps/test.php",
-                data: { nom: nom, mail: mail, tel: tel}
+                data: { nom: nom, mail: mail, tel: tel }
             })
             .done(function( msg ) {
-                alert( "Data Saved: " + msg );
+                if(msg == 1){
+                    fileTransfer.subirFoto();
+                }
             });
         }else{
-            navigator.notification.alert('Todos los campos son requeridos',null,'Lo sentimos','Aceptar');      
+            navigator.notification.alert('Todos los campos son requeridos',null,'Lo sentimos','Aceptar');
         }
     }
 };
--- js/api/capture.js
+++ js/api/capture.js
@@ -1,15 +1,15 @@
 var capture = {
     imgTomada: function(imgs){
         var image = '';
-        for(i = 0; i < imgs.length;i++)
+        for(i = 0;i < imgs.length;i++)
             image = imgs[i].fullPath;
         $('#regFoto').attr('rel',image);
-        $('#viewPhoto').html('<img scr="' + image + '" style="width:100%;">');
+        $('#viewPhoto').html('<img src="' + image + '" style="width:100%;">');
     },
     imgError: function(err){
-        navigator.notification.alert('Error: '+err.code,null,'lo sentimos','aceptar');        
+        navigator.notification.alert('Error: '+err.code,null,'Lo sentimos','Aceptar');
     },
     imgCapture: function(){
-        navigator.device.capture.captureImage(capture.imgTomada, capture.imgError, {limit:2});
+        navigator.device.capture.captureImage(capture.imgTomada, capture.imgError);
     }
 }
